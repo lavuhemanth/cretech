a34ddfe31a43ecc663e74f1f3a6a863ae14ef27e
hemanthlavu <hemanthkumar.lavu@contus.in>
2022-11-15 10:08:14 +0530

    " Addind code "

---
+++ src/components/Contact/Main.js
@@ -0,0 +1,236 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import BG from '../../assets/images/background/contact-one-bg-1-1.png'
+import Breadcumb from '../Breadcumb/Main';
+import emailjs from "emailjs-com";
+import { ToastContainer, toast } from "react-toastify";
+  import "react-toastify/dist/ReactToastify.css";
+
+
+const Contact = () => {
+
+  const [email, setEmail] = useState("");
+  const [body, setBody] = useState("")
+  const [name, setName] = useState("");
+    const handleSubmit = (event) =>  {
+      event.preventDefault();
+      debugger
+      emailjs.send("service_dklncb9", "template_8bmuiyv", { name, email, body }, '0wY3XRYMuz7nWwg0I').then(data => {
+        console.log('data :: ', data)
+        if (data.status === 200) {
+          toast.success("Mail Sent successfully", {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+          });
+        }
+        setEmail('');
+        setBody('');
+        setName('');
+      })
+        .catch(error => {
+          console.log(error)
+          toast("Something went wrong");
+          toast.warn("Something went wrong!", {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+          });
+        });
+    };
+    return (
+      <>
+        <ToastContainer />
+        <Breadcumb Title="Contact" Breadcumb="CONTACT" />
+        <section className="contact-one section-padding--top section-padding--bottom">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8">
+                <form
+                  className="contact-one__form contact-form-validated"
+                  name="contact"
+                >
+                  <div className="section-title">
+                    <p className="section-title__text">Contact with us</p>
+                    <h2 className="section-title__title">
+                      Join Us To Get IT Free Consultations
+                    </h2>
+                  </div>
+                  <div className="row ">
+                    <div className="col-lg-6 col-md-12">
+                      <input
+                        type="text"
+                        placeholder="Your name"
+                        name="name"
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="col-lg-6 col-md-12">
+                      <input
+                        type="email"
+                        placeholder="Email address"
+                        name="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="col-lg-12 col-md-12">
+                      <textarea
+                        name="body"
+                        placeholder="Write message"
+                        onChange={(e) => setBody(e.target.value)}
+                      ></textarea>
+                    </div>
+                    <div className="col-md-12">
+                      <button
+                        className="thm-btn contact-one__btn"
+                        onClick={handleSubmit}
+                        type="submit"
+                      >
+                        <span>send a message</span>
+                      </button>
+                    </div>
+                  </div>
+                </form>
+                <div className="result"></div>
+              </div>
+              <div className="col-lg-4">
+                <div
+                  className="contact-one__info wow fadeInRight"
+                  data-wow-duration="1500ms"
+                  style={{ backgroundImage: `url(${BG})` }}
+                >
+                  <div className="contact-one__item">
+                    <h3 className="contact-one__item__title">Address</h3>
+                    <p className="contact-one__item__text">
+                      80 Atlantic Avenue, Toronto, ON M6K 1X9
+                      <br />
+                      Canada
+                    </p>
+                  </div>
+                  {/* <div className="contact-one__item">
+                    <h3 className="contact-one__item__title">Address</h3>
+                    <p className="contact-one__item__text">
+                      330 5th Avenue SW, Suite 1800, Calgary, T2P 0J4
+                      <br />
+                      Canada
+                    </p>
+                  </div>
+                  <div className="contact-one__item">
+                    <h3 className="contact-one__item__title">Address</h3>
+                    <p className="contact-one__item__text">
+                      385 Rue Grenadier, Saint Constant, J5A 0L1
+                      <br />
+                      Canada
+                    </p>
+                  </div> */}
+                  {/* <div className="contact-one__item">
+                    <h3 className="contact-one__item__title">Phone</h3>
+                    <p className="contact-one__item__text">
+                      Local: <Link to="tel:222999888">222 999 888</Link>
+                      <br />
+                      Mobile: <Link to="tel:0008888999">000 8888 999</Link>
+                    </p>
+                  </div> */}
+                  <div className="contact-one__item">
+                    <h3 className="contact-one__item__title">Email</h3>
+                    <p className="contact-one__item__text">
+                      <Link to="mailto:info@sreeson.ca">info@sreeson.ca</Link>
+                      <br />
+                    </p>
+                  </div>
+                  <div className="contact-one__item">
+                    <h3 className="contact-one__item__title">Mobile</h3>
+                    <p className="contact-one__item__text">
+                      <span>
+                        <b>INDIA</b> +91 9972392949
+                      </span>
+                      <br />
+                      <span>
+                        <b>CANADA</b> +1 (514) 623-8550
+                      </span>
+                      <br />
+                    </p>
+                  </div>
+                  <div className="contact-one__item">
+                    <ul className="contact-one__social">
+                      <li>
+                        <Link
+                          to="#"
+                          onClick={() =>
+                            window.location.assign(
+                              "https://twitter.com/SreesonCanada"
+                            )
+                          }
+                        >
+                          <i className="fab fa-twitter"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="#"
+                          onClick={() =>
+                            window.location.assign(
+                              "https://www.facebook.com/SreesonCanada/"
+                            )
+                          }
+                        >
+                          <i className="fab fa-facebook-f"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="#"
+                          onClick={() =>
+                            window.location.assign(
+                              "https://www.linkedin.com/company/sreeson-canada-inc/"
+                            )
+                          }
+                        >
+                          <i className="fa-brands fa-linkedin"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="#"
+                          onClick={() =>
+                            window.location.assign(
+                              "https://www.instagram.com/sreeson_canada/"
+                            )
+                          }
+                        >
+                          <i className="fab fa-instagram"></i>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="google-map google-map--contact">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2887.456348845883!2d-79.42340808411676!3d43.638672979121814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b3506cbb2ecc9%3A0x1660f18bd29ddb35!2s80%20Atlantic%20Ave%2C%20Toronto%2C%20ON%20M6K%201X9%2C%20Canada!5e0!3m2!1sen!2sin!4v1668097536527!5m2!1sen!2sin"
+            className="google-map__one"
+            title="gmap"
+            allowFullScreen
+          ></iframe>
+        </section>
+      </>
+    );
+}
+
+export default Contact
---
+++ src/components/Navbar/Main.js
@@ -0,0 +1,277 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import Logo from '../../assets/images/logo-dark.png'
+import WOW from 'wowjs'
+
+const Navbar = () => {
+    const location = useLocation()
+    const path = location.pathname
+    const [sticky, setSticky] = useState(false)
+    const [search, setSearch] = useState(false)
+
+    const [menu, setmenu] = useState({})
+
+    const activeMenu = () => {
+        if(path === "/" || path === "/home-02" || path === "/home-03" ){
+            setmenu({home:true})
+        }else if(path === "/about" || path === "/team"){
+            setmenu({pages:true})
+        }else if(path === "/service-01" || path === "/service-02" || path === "qa-testing" || path === "/it-management" || path === "cyber-security" || path === "/it-consultant" || path === "/infrastructure-plan"){
+            setmenu({services:true})
+        }else if(path === "/projects" || path === "/project-details"){
+            setmenu({project:true})
+        }else if(path === "/blog" || path === "/blog-details"){
+            setmenu({blog:true})
+        }else if(path === "/contact"){
+            setmenu({contact:true})
+        }else{
+            setmenu({home:true})
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", isSticky);
+        return () => {
+            window.removeEventListener("scroll", isSticky);
+        };
+    }, []);
+
+    // useEffect(()=>{
+    //     window.scroll(0, 0)
+    //     new WOW.WOW({
+    //       live: false
+    //     }).init();
+    //     activeMenu()
+    // }, [path])
+    
+    const isSticky = () => {
+        const scrollTop = window.scrollY;
+        scrollTop >= 141 ? setSticky(true) : setSticky(false);
+  }
+  
+  const navigate = () => {
+    window.location.assign("https://calendly.com/sreeson");
+  }
+    return (
+      <>
+        <div className="topbar">
+          <div className="container-fluid">
+            <p className="topbar__text">Welcome to CreTech</p>
+            <ul className="topbar__info">
+              <li>
+                <i className="fa fa-envelope"></i>
+                <Link to="mailto:needhelp@company.com">info@sreeson.ca</Link>
+              </li>
+              <li>
+                <i className="fa fa-map-marker"></i>
+                80 Atlantic Avenue, Toronto, ON M6K 1X9, Canada
+              </li>
+            </ul>
+            <ul className="topbar__social">
+              <li>
+                <Link
+                  to="#"
+                  onClick={() =>
+                    window.location.assign("https://twitter.com/SreesonCanada")
+                  }
+                >
+                  <i className="fab fa-twitter"></i>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="#"
+                  onClick={() =>
+                    window.location.assign(
+                      "https://www.facebook.com/SreesonCanada/"
+                    )
+                  }
+                >
+                  <i className="fab fa-facebook"></i>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="#"
+                  onClick={() =>
+                    window.location.assign(
+                      "https://www.linkedin.com/company/sreeson-canada-inc/"
+                    )
+                  }
+                >
+                  <i class="fa-brands fa-linkedin"></i>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="#"
+                  onClick={() =>
+                    window.location.assign(
+                      "https://www.instagram.com/sreeson_canada/"
+                    )
+                  }
+                >
+                  <i className="fab fa-instagram"></i>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <nav
+          className={`main-menu sticky-header ${
+            sticky && "sticky-header--cloned sticky-fixed"
+          }`}
+        >
+          <div className="container-fluid">
+            <div className="main-menu__logo">
+              <Link to="/">
+                <img src={Logo} width="98" height="33" alt="Cretech" />
+              </Link>
+            </div>
+
+            <ul className="main-menu__list">
+              <li
+                className={`menu-item-has-children ${menu.home && "current"}`}
+              >
+                <Link to="/">Home</Link>
+                {/* <ul>
+                                <li><Link to="/">Home One</Link></li>
+                                <li><Link to="/home-02">Home Two</Link></li>
+                                <li><Link to="/home-03">Home Three</Link></li>
+                                <li className="menu-item-has-children"><Link to="/">Header Styles</Link>
+                                    <ul>
+                                        <li><Link to="/">Header One</Link></li>
+                                        <li><Link to="/home-02">Header Two</Link></li>
+                                        <li><Link to="/home-03">Header Three</Link></li>
+                                    </ul>
+                                </li>
+                            </ul> */}
+              </li>
+              <li
+                className={`menu-item-has-children ${menu.pages && "current"}`}
+              >
+                <Link to="/about">About Us</Link>
+                {/* <ul>
+                  <li>
+                    <Link to="/about">About Us</Link>
+                  </li>
+                  <li>
+                    <Link to="/team">Our Team</Link>
+                  </li>
+                </ul> */}
+              </li>
+              <li
+                className={`menu-item-has-children ${
+                  menu.services && "current"
+                }`}
+              >
+                <Link to="/services">Services</Link>
+                <ul>
+                  {/* <li>
+                    <Link to="/service-01">Services 01</Link>
+                  </li>
+                  <li>
+                    <Link to="/service-02">Services 02</Link>
+                  </li> */}
+                  <li>
+                    <Link to="/cyber-security">Cyber Security</Link>
+                  </li>
+                  <li>
+                    <Link to="/it-management">IT Management</Link>
+                  </li>
+                  <li>
+                    <Link to="/qa-testing">QA & Testing</Link>
+                  </li>
+                  <li>
+                    <Link to="/infrastructure-plan">Infrastructure Plan</Link>
+                  </li>
+                  <li>
+                    <Link to="/it-consultant">IT Consultant</Link>
+                  </li>
+                </ul>
+              </li>
+              {/* <li
+                className={`menu-item-has-children ${
+                  menu.project && "current"
+                }`}
+              >
+                <Link to="/projects">Projects</Link>
+                <ul>
+                  <li>
+                    <Link to="/projects">Projects</Link>
+                  </li>
+                  <li>
+                    <Link to="/project-details">Projects Details</Link>
+                  </li>
+                </ul>
+              </li>
+              <li
+                className={`menu-item-has-children ${menu.blog && "current"}`}
+              >
+                <Link to="/blog">Blog</Link>
+                <ul>
+                  <li>
+                    <Link to="/blog">Blog</Link>
+                  </li>
+                  <li>
+                    <Link to="/blog-details">Blog Details</Link>
+                  </li>
+                </ul>
+              </li> */}
+              <li
+                className={`menu-item-has-children ${
+                  menu.contact && "current"
+                }`}
+              >
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+
+            <div className="main-menu__right">
+              {/* <Link to="#" className="mobile-nav__toggler">
+                <span></span>
+                <span></span>
+                <span></span>
+              </Link>
+              <Link
+                to="#"
+                onClick={() => setSearch(true)}
+                className="search-toggler"
+              >
+                <i className="icon-magnifying-glass"></i>
+              </Link> */}
+              <Link to="#" className="main-menu__cta" onClick={navigate}>
+                <i className="fa fa-clock"></i>
+                <span className="main-menu__cta__text">Schedule a Meeting</span>
+              </Link>
+            </div>
+          </div>
+        </nav>
+        <div className={`search-popup ${search && "active"}`}>
+          <div
+            className="search-popup__overlay search-toggler"
+            onClick={() => setSearch(false)}
+          ></div>
+          <div className="search-popup__content">
+            <form action="#">
+              <label htmlFor="search" className="sr-only">
+                search here
+              </label>
+              <input type="text" id="search" placeholder="Search Here..." />
+              <button
+                type="submit"
+                aria-label="search submit"
+                className="thm-btn"
+              >
+                <span>
+                  <i className="icon-magnifying-glass"></i>
+                </span>
+              </button>
+            </form>
+          </div>
+        </div>
+      </>
+    );
+}
+
+export default Navbar
